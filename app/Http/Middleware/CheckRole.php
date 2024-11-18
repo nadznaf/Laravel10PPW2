968e7da9de86c9eb1e9d36abeedf1614c9a70320
nadznaf <nadziraazhanifarahiya@mail.ugm.ac.id>
2024-11-18 22:58:03 +0700

    add middleware checkage and checkrole

---
+++ app/Http/Middleware/CheckRole.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckRole
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if ($request->role !== 'admin') {
+            return redirect()->route('welcome')
+                ->withError('You must be an admin to access this page.');
+        }
+        return $next($request);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Auth\LoginRegisterController;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
 
 Route::controller(LoginRegisterController::class)->group(function () {
     Route::get('/register', 'register')->name('register');
@@ -16,3 +16,13 @@ Route::controller(LoginRegisterController::class)->group(function () {
     Route::get('/dashboard', 'dashboard')->name('dashboard');
     Route::post('/logout', 'logout')->name('logout');
 });
+
+Route::get('checkage', function () {
+    return redirect()->route('dashboard')
+        ->withSuccess('You are 18 yeard old or older.');
+})->middleware('checkage');
+
+Route::get('checkrole', function () {
+    return redirect()->route('dashboard')
+        ->withSuccess('You are an admin.');
+})->middleware('checkrole');
